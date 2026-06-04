cc8b16d5e3d443be7dea561a2a55900760317211
hannerco <hanner.condeo@unac.edu.co>
2026-06-03 20:33:18 -0500

    DeckService

---
+++ backend/src/main/java/edu/unac/service/DeckService.java
@@ -0,0 +1,11 @@
+package edu.unac.service;
+
+import edu.unac.model.card.Card;
+
+import java.util.List;
+
+public interface DeckService {
+
+    List<Card> buildDeck();
+
+}
---
+++ backend/src/main/java/edu/unac/service/impl/DeckServiceImpl.java
@@ -0,0 +1,64 @@
+package edu.unac.service.impl;
+
+import edu.unac.model.card.*;
+import edu.unac.model.enums.ModifierType;
+import edu.unac.service.DeckService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class DeckServiceImpl implements DeckService {
+
+    @Override
+    public List<Card> buildDeck() {
+
+        List<Card> deck = new ArrayList<>();
+
+        addNumericCards(deck);
+
+        addActionCards(deck);
+
+        addModifierCards(deck);
+
+        Collections.shuffle(deck);
+
+        return deck;
+    }
+
+    private void addNumericCards(List<Card> deck) {
+
+        deck.add(new NumericCard(0));
+        deck.add(new NumericCard(1));
+
+        for (int value = 2; value <= 12; value++) {
+
+            for (int count = 0; count < value; count++) {
+
+                deck.add(new NumericCard(value));
+            }
+        }
+    }
+
+    private void addActionCards(List<Card> deck) {
+
+        for (int i = 0; i < 3; i++) {
+
+            deck.add(new FreezeCard());
+            deck.add(new FlipThreeCard());
+            deck.add(new SecondChanceCard());
+        }
+    }
+
+    private void addModifierCards(List<Card> deck) {
+
+        deck.add(new ModifierCard(ModifierType.PLUS_2));
+        deck.add(new ModifierCard(ModifierType.PLUS_4));
+        deck.add(new ModifierCard(ModifierType.PLUS_6));
+        deck.add(new ModifierCard(ModifierType.PLUS_8));
+        deck.add(new ModifierCard(ModifierType.PLUS_10));
+        deck.add(new ModifierCard(ModifierType.TIMES_2));
+    }
+}
